2e4d7bcd4875ae7b6483b0b0eb4100d0ea0d8112
Ankit Rana <ankitrana.95@outlook.com>
2022-02-10 01:39:58 +0530

    added natural number using recursion

--- src/recursion/NaturalNumbers.java
+++ src/recursion/NaturalNumbers.java
@@ -1,7 +1,35 @@
 package recursion;
 
 public class NaturalNumbers {
-    public static void main(String[] args) {
+    //Print natural numbers N to 1
+    private static void print(int n) {
+        if(n == 0)
+            return;
+        System.out.print(n + " ");
+        print(n -1);
+    }
+// Print 1 to N using backtracking
+    private static void print2(int n) {
+        if(n == 0)
+            return;
+        print2(n-1);
+        System.out.print(n + " ");
+    }
+
+    //Print 1 to N
+    private static void print3(int i, int n) {
+        if(i > n)
+            return;
+        System.out.print(i + " ");
+        print3(i+1, n);
 
     }
+
+    public static void main(String[] args) {
+        print(5);
+        System.out.println();
+        print2(5);
+        System.out.println();
+        print3(1,5);
+    }s
 }
