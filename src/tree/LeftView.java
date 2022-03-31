d87349bc502c4182be6c71827662ca0390778c36
Ankit Rana <ankitrana.95@outlook.com>
2022-03-31 23:59:39 +0530

    Add recursive approach for right and left view

--- src/tree/LeftView.java
+++ src/tree/LeftView.java
@@ -2,6 +2,7 @@ package tree;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class LeftView {
@@ -31,9 +32,22 @@ public class LeftView {
             System.out.print(i + " ");
     }
 
+    //Better space complexity
+    private static void recursiveLeftView(TreeNode root, List<Integer> res, int level) {
+        if(root == null)
+            return;
+        if(level == res.size())
+            res.add(root.data);
+        recursiveLeftView(root.left, res, level + 1);
+        recursiveLeftView(root.right, res, level + 1);
+    }
+
     public static void main(String[] args) {
-        leftView(TreeUtil.getTree2());
-        System.out.println();
         leftView(TreeUtil.getTree());
+        System.out.println();
+        List<Integer> res = new ArrayList<>();
+        recursiveLeftView(TreeUtil.getTree(), res, 0);
+        for(Integer i : res)
+            System.out.print(i + " ");
     }
 }
--- src/tree/RightView.java
+++ src/tree/RightView.java
@@ -2,6 +2,7 @@ package tree;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class RightView {
@@ -31,9 +32,22 @@ public class RightView {
             System.out.print(i + " ");
     }
 
+    //Better space complexity
+    private static void recursiveRightView(TreeNode root, List<Integer> res, int level) {
+        if(root == null)
+            return;
+        if(level == res.size())
+            res.add(root.data);
+        recursiveRightView(root.right, res, level + 1);
+        recursiveRightView(root.left, res, level + 1);
+    }
+
     public static void main(String[] args) {
         rightView(TreeUtil.getTree2());
         System.out.println();
-        rightView(TreeUtil.getTree());
+        List<Integer> res = new ArrayList<>();
+        recursiveRightView(TreeUtil.getTree2(), res, 0);
+        for(Integer i : res)
+            System.out.print(i + " ");
     }
 }
