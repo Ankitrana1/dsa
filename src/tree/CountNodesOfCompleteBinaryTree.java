332ccd96ba69d105c8b9e04f740805da244ffd6f
Ankit Rana <ankitrana.95@outlook.com>
2022-04-06 00:07:21 +0530

    count nodes in complete binary tree (logN)

---
+++ src/tree/CountNodesOfCompleteBinaryTree.java
@@ -0,0 +1,36 @@
+package tree;
+
+public class CountNodesOfCompleteBinaryTree {
+
+    private static int countNodes(TreeNode root) {
+        if(root == null) return 0;
+        int lh = getLeftHeight(root);
+        int rh = getRightHeight(root);
+
+        if(lh == rh)
+            return ((2 << (lh-1)) - 1);
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+
+    private static int getLeftHeight(TreeNode node) {
+        int count = 0;
+        while(node != null){
+            node = node.left;
+            count++;
+        }
+        return count;
+    }
+
+    private static int getRightHeight(TreeNode node) {
+        int count = 0;
+        while(node != null){
+            node = node.right;
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countNodes(TreeUtil.getCompleteBinaryTree()));
+    }
+}
--- src/tree/TreeUtil.java
+++ src/tree/TreeUtil.java
@@ -50,9 +50,10 @@ public class TreeUtil {
                      1
                   2     3
                 4   5|6    9
-                  7    8
+                  7   8
          */
     }
+
     public static TreeNode getTree3() {
         TreeNode root = new TreeNode(1);
         TreeNode leaf4 = new TreeNode(7);
@@ -73,6 +74,37 @@ public class TreeUtil {
                   2     3
                 4         8
               7             9
+         */
+    }
+
+    public static TreeNode getCompleteBinaryTree() {
+        TreeNode root = new TreeNode(1);
+        TreeNode leaf4 = new TreeNode(7);
+        TreeNode leaf42 = new TreeNode(8);
+        TreeNode leaf43 = new TreeNode(8);
+        TreeNode l32 = new TreeNode(5);
+        l32.left = leaf4;
+        l32.right = leaf42;
+        TreeNode leaf31 = new TreeNode(4);
+        TreeNode leaf41 = new TreeNode(6);
+        leaf31.left = leaf41;
+        leaf31.right = leaf43;
+        TreeNode leaf34 = new TreeNode(9);
+        TreeNode leaf33 = new TreeNode(6);
+        TreeNode l21 = new TreeNode(2);
+        l21.left = leaf31;
+        l21.right = l32;
+        TreeNode l22 = new TreeNode(3);
+        l22.left = leaf33;
+        l22.right = leaf34;
+        root.left = l21;
+        root.right = l22;
+        return root;
+        /*
+                     1
+                  2     3
+                4   5|6    9
+              6  8 7  8
          */
     }
 }
